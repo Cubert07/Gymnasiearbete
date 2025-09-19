fada9d072553314f1b240bc74d5227d238687215
Cubert <97406764+Cubert07@users.noreply.github.com>
2025-09-19 11:42:43 +0000

    försök på wavy Lines

--- scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.dropdown-toggle');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    toggleButton.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('active');
-    });
-});
