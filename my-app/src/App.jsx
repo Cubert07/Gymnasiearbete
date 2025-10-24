c6cb597622cd3573670d0a687303937be77a7312
Cuber07 <albertahlqvist@gmail.com>
2025-10-24 13:29:08 +0200

    instalerade och skapade projekt

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Button variant="contained">Hello MUI</Button>
+    </div>
+  );
+}
+
+export default App;
