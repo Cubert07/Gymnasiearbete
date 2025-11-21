577090ac70bbeefa0b50680d8519418ce8b3677b
Cuber07 <albertahlqvist@gmail.com>
2025-11-21 14:10:53 +0100

    Byggt lite på start sidan

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -5,9 +5,30 @@ import { Box, Typography } from '@mui/material';
 export default function Home() {
     return (
         <>
-        <Box sx={{ p:4 }} component="main">
-            <Typography variant="h1">
-            </Typography>
+        <Box 
+            sx={{ 
+                backgroundImage: "url('https://placehold.co/1920x400')", 
+                height: '400px',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                }}>
+            <Box 
+            sx={{ 
+                width: '50vw',
+                float: 'left',
+                pl: '5vw',
+             }}>
+                <Typography variant="h1">
+                    <h5 style={{ paddingBottom: '0 vh' }}>
+                        Full kontroll.
+                    <br />
+                        Högsta trygghet.</h5>
+                    <p style={{ fontSize: '5vh' }}>Joysticks som ger frihet att röra sig.</p>
+
+
+                </Typography>
+            </Box>
         </Box>
         </>
     );
